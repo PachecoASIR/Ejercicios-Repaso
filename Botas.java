2bd59b1dfac2fdc046df5aa6f49d6ffbf68f91a2
PachecoASIR <jferradaspacheco@danielcastelao.org>
2023-03-19 21:23:49 +0100

    Ejercicio 5 v1

---
+++ Botas.java
@@ -0,0 +1,13 @@
+public class Botas extends Armadura{
+
+    public Botas(){
+    }
+
+    public Botas(double defensa, double multiplicador){
+        super(defensa,multiplicador);
+    }
+
+       public String accionEspecial (boolean meele, int distancia){
+        return "";
+    }
+}
